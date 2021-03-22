a9b73fc76ba8631df9751bc7b929eac743cc975c
Tanim <42853370+tanim913@users.noreply.github.com>
2021-03-22 20:29:55 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyHomePage extends StatelessWidget {
     Transaction(
         title: 'New Phone', id: 'T1', amount: 899.00, date: DateTime.now()),
     Transaction(
-        title: 'New PC', id: 'T2', amount: 8889.00, date: DateTime.now())
+        title: 'New PC', id: 'T2', amount: 889.0, date: DateTime.now())
   ];
   @override
   Widget build(BuildContext context) {
@@ -29,20 +29,50 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
+        children: [
           Container(
             width: double.infinity,
             height: 50,
             child: Card(
               color: Colors.blue,
-              child: Container(child: Text('Chart!')),
+              child: Container(
+                child: Text('Chart!', textAlign: TextAlign.center),
+              ),
               elevation: 7,
             ),
           ),
           Column(
               children: transaction.map((tx) {
             return Card(
-              child: Text(tx.title),
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple),
+                      )),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold, color: Colors.indigoAccent),
+                      ),
+                      Text(tx.date.toString(), style: TextStyle (color: Colors.grey),),
+                    ],
+                  ),
+                ],
+              ),
             );
           }).toList())
         ],
