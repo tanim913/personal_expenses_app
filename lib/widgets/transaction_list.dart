185c2d9b089b7cb208729a1c39d931092672fe5d
Tanim <42853370+tanim913@users.noreply.github.com>
2021-03-26 21:50:25 +0600

    Update transaction_list.dart

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,49 +3,53 @@ import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
-
-  final List <Transaction> transactions;
+  final List<Transaction> transactions;
   TransactionList(this.transactions);
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-              children: transactions.map((tx) {
-            return Card(
-              color: Colors.white,
-              child: Row(
-                children: [
-                  Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.purple, width: 2)),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${tx.amount}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple),
-                      )),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(tx.date),
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }).toList());
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+              child: Column(
+            children: transactions.map((tx) {
+          return Card(
+            color: Colors.white,
+            child: Row(
+              children: [
+                Container(
+                    margin: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${tx.amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    )),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tx.title,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(tx.date),
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }).toList()),
+      ),
+    );
   }
 }
