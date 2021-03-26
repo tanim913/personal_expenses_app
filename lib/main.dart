7683f349957231499916626b4e2dae97b4d90917
Tanim <42853370+tanim913@users.noreply.github.com>
2021-03-26 21:38:18 +0600

    adding transactions by user input
    adding scroll view and separating to different organized widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import './transaction.dart';
+import './widgets/user_transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,93 +14,31 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transaction = [
-    Transaction(
-        title: 'New Phone', id: 'T1', amount: 899.00, date: DateTime.now()),
-    Transaction(title: 'New PC', id: 'T2', amount: 889.0, date: DateTime.now())
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Personal Expenses'),
       ),
-      body: Column(
-        //mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            width: double.infinity,
-            height: 40,
-            child: Card(
-              color: Colors.blue,
-              child: Container(
-                child: Text('Chart!', textAlign: TextAlign.center),
-              ),
-              elevation: 7,
-            ),
-          ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
+      body: SingleChildScrollView(
               child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                  ),
-                  ElevatedButton(
-                    
-                    onPressed: () {},
-                    child: Text('Add Transaction',style: TextStyle(color: Colors.black),),
-                  )
-                ],
+          //mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 40,
+              child: Card(
+                color: Colors.blue,
+                child: Container(
+                  child: Text('Chart!', textAlign: TextAlign.center),
+                ),
+                elevation: 7,
               ),
             ),
-          ),
-          Column(
-              children: transaction.map((tx) {
-            return Card(
-              color: Colors.white,
-              child: Row(
-                children: [
-                  Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.purple, width: 2)),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${tx.amount}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple),
-                      )),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black),
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(tx.date),
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }).toList())
-        ],
+            UserTransaction(),
+          ],
+        ),
       ),
     );
   }
